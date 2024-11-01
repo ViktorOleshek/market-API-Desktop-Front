c9c2cd46b7590df3afdb3fc55c4f4739d1b06e4a
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-11-02 01:15:30 +0200

    Implement controllers in PL

--- WebApi/Controllers/CustomersController.cs
+++ WebApi/Controllers/CustomersController.cs
@@ -1,61 +1,119 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Business.Interfaces;
-using Business.Models;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-
-namespace WebApi.Controllers
+﻿namespace WebApi.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class CustomersController : ControllerBase
-    {
-        private readonly ICustomerService _customerService;
+	using System;
+	using System.Collections.Generic;
+	using System.ComponentModel.DataAnnotations;
+	using System.Threading.Tasks;
+	using Business.Interfaces;
+	using Business.Models;
+	using Microsoft.AspNetCore.Mvc;
 
-        //Inject customer service via constructor
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CustomersController : ControllerBase
+	{
+		private readonly ICustomerService _customerService;
 
-        // GET: api/customers
-        [HttpGet]
-        public async Task<ActionResult<IEnumerable<CustomerModel>>> Get()
-        {
-            throw new NotImplementedException();
-        }
+		public CustomersController(ICustomerService customerService)
+		{
+			_customerService = customerService;
+		}
 
-        //GET: api/customers/1
-        [HttpGet("{id}")]
-        public async Task<ActionResult<CustomerModel>> GetById(int id)
-        {
-            throw new NotImplementedException();
-        }
-        
-        //GET: api/customers/products/1
-        [HttpGet("products/{id}")]
-        public async Task<ActionResult<CustomerModel>> GetByProductId(int id)
-        {
-            throw new NotImplementedException();
-        }
+		// GET: api/customers
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<CustomerModel>>> Get()
+		{
+			var customers = await _customerService.GetAllAsync();
+			return Ok(customers);
+		}
 
-        // POST: api/customers
-        [HttpPost]
-        public async Task<ActionResult> Post([FromBody] CustomerModel value)
-        {
-            throw new NotImplementedException();
-        }
+		// GET: api/customers/1
+		[HttpGet("{id}")]
+		public async Task<ActionResult<CustomerModel>> GetById(int id)
+		{
+			var customer = await _customerService.GetByIdAsync(id);
+			if (customer == null)
+			{
+				return NotFound();
+			}
+			return Ok(customer);
+		}
 
-        // PUT: api/customers/1
-        [HttpPut("{id}")]
-        public async Task<ActionResult> Put(int Id, [FromBody] CustomerModel value)
-        {
-            throw new NotImplementedException();
-        }
+		// GET: api/customers/products/1
+		[HttpGet("products/{id}")]
+		public async Task<ActionResult<IEnumerable<CustomerModel>>> GetByProductId(int id)
+		{
+			var customers = await _customerService.GetCustomersByProductIdAsync(id);
+			return Ok(customers);
+		}
 
-        // DELETE: api/customers/1
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
+		// POST: api/customers
+		[HttpPost]
+		public async Task<ActionResult<CustomerModel>> Post([FromBody] CustomerModel value)
+		{
+			if (!IsValidCustomer(value))
+			{
+				return BadRequest(ModelState);
+			}
+
+			await _customerService.AddAsync(value);
+
+			return CreatedAtAction(nameof(GetById), new { id = value.Id }, value);
+		}
+
+		// PUT: api/customers/1
+		[HttpPut("{id}")]
+		public async Task<ActionResult> Put(int id, [FromBody] CustomerModel value)
+		{
+			if (id != value.Id)
+			{
+				return BadRequest();
+			}
+
+			if (!IsValidCustomer(value))
+			{
+				return BadRequest(ModelState);
+			}
+
+			await _customerService.UpdateAsync(value);
+			return NoContent();
+		}
+
+		// DELETE: api/customers/1
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			await _customerService.DeleteAsync(id);
+			return NoContent();
+		}
+
+		private bool IsValidCustomer(CustomerModel customer)
+		{
+			if (string.IsNullOrWhiteSpace(customer.Name))
+			{
+				ModelState.AddModelError(nameof(customer.Name), "Name is required");
+				return false;
+			}
+
+			if (string.IsNullOrWhiteSpace(customer.Surname))
+			{
+				ModelState.AddModelError(nameof(customer.Surname), "Surname is required");
+				return false;
+			}
+
+			if (customer.BirthDate > DateTime.Now || customer.BirthDate.Year < 1900)
+			{
+				ModelState.AddModelError(nameof(customer.BirthDate), "Birth date is not valid");
+				return false;
+			}
+
+			if (customer.DiscountValue < 0)
+			{
+				ModelState.AddModelError(nameof(customer.DiscountValue), "Discount value cannot be negative");
+				return false;
+			}
+
+			return true;
+		}
+	}
 }
---
+++ WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,145 @@
+﻿namespace WebApi.Controllers
+{
+	using System.Collections.Generic;
+	using System.Threading.Tasks;
+	using Business.Interfaces;
+	using Business.Models;
+	using Microsoft.AspNetCore.Mvc;
+
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ProductsController : ControllerBase
+	{
+		private readonly IProductService _productService;
+
+		public ProductsController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
+		// GET: api/products
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<ProductModel>>> Get([FromQuery] FilterSearchModel filterSearch)
+		{
+			var products = await _productService.GetByFilterAsync(filterSearch);
+			return Ok(products);
+		}
+
+		// GET: api/products/{id}
+		[HttpGet("{id}")]
+		public async Task<ActionResult<ProductModel>> GetById(int id)
+		{
+			var product = await _productService.GetByIdAsync(id);
+			if (product == null)
+			{
+				return NotFound();
+			}
+
+			return Ok(product);
+		}
+
+		// POST: api/products
+		[HttpPost]
+		public async Task<ActionResult<ProductModel>> Post([FromBody] ProductModel productModel)
+		{
+			if (!IsValidProduct(productModel))
+			{
+				return BadRequest(ModelState);
+			}
+
+			await _productService.AddAsync(productModel);
+			return CreatedAtAction(nameof(GetById), new { id = productModel.Id }, productModel);
+		}
+
+		// PUT: api/products/{id}
+		[HttpPut("{id}")]
+		public async Task<ActionResult> Put(int id, [FromBody] ProductModel productModel)
+		{
+			if (!IsValidProduct(productModel) || id != productModel.Id)
+			{
+				return BadRequest(ModelState);
+			}
+
+			await _productService.UpdateAsync(productModel);
+			return NoContent();
+		}
+
+		// DELETE: api/products/{id}
+		[HttpDelete("{id}")]
+		public async Task<ActionResult> Delete(int id)
+		{
+			await _productService.DeleteAsync(id);
+			return NoContent();
+		}
+
+		// GET: api/products/categories
+		[HttpGet("categories")]
+		public async Task<ActionResult<IEnumerable<ProductCategoryModel>>> GetCategories()
+		{
+			var categories = await _productService.GetAllProductCategoriesAsync();
+			return Ok(categories);
+		}
+
+		// POST: api/products/categories
+		[HttpPost("categories")]
+		public async Task<ActionResult<ProductCategoryModel>> PostCategory([FromBody] ProductCategoryModel categoryModel)
+		{
+			if (!IsValidCategory(categoryModel))
+			{
+				return BadRequest(ModelState);
+			}
+
+			await _productService.AddCategoryAsync(categoryModel);
+			return CreatedAtAction(nameof(GetCategories), new { id = categoryModel.Id }, categoryModel);
+		}
+
+		// PUT: api/products/categories/{id}
+		[HttpPut("categories/{id}")]
+		public async Task<ActionResult> PutCategory(int id, [FromBody] ProductCategoryModel categoryModel)
+		{
+			if (!IsValidCategory(categoryModel) || id != categoryModel.Id)
+			{
+				return BadRequest(ModelState);
+			}
+
+			await _productService.UpdateCategoryAsync(categoryModel);
+			return NoContent();
+		}
+
+		// DELETE: api/products/categories/{id}
+		[HttpDelete("categories/{id}")]
+		public async Task<ActionResult> DeleteCategory(int id)
+		{
+			await _productService.RemoveCategoryAsync(id);
+			return NoContent();
+		}
+
+		private bool IsValidProduct(ProductModel product)
+		{
+			if (string.IsNullOrWhiteSpace(product.ProductName))
+			{
+				ModelState.AddModelError(nameof(product.ProductName), "Name is required.");
+				return false;
+			}
+
+			if (product.Price < 0)
+			{
+				ModelState.AddModelError(nameof(product.Price), "Price cannot be negative.");
+				return false;
+			}
+
+			return true;
+		}
+
+		private bool IsValidCategory(ProductCategoryModel category)
+		{
+			if (string.IsNullOrWhiteSpace(category.CategoryName))
+			{
+				ModelState.AddModelError(nameof(category.CategoryName), "Category name is required.");
+				return false;
+			}
+
+			return true;
+		}
+	}
+}
