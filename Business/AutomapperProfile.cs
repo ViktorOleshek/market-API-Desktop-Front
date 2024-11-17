0a3892b7a8cf3fce77d6aaebec2eab9a94814c83
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-11-17 11:28:52 +0200

    Move AutoMapper from DAL to BLL

--- Business/AutomapperProfile.cs
+++ Business/AutomapperProfile.cs
@@ -1,40 +1,35 @@
 ﻿using System.Linq;
+using Abstraction.IEntities;
 using Abstraction.Models;
 using AutoMapper;
-using Data.Entities;
 
-namespace Data
+namespace Business
 {
     public class AutomapperProfile : Profile
     {
         public AutomapperProfile()
         {
-            this.CreateMap<Receipt, ReceiptModel>()
-                .ForMember(rm => rm.Id, c => c.MapFrom(r => r.Id))
+            this.CreateMap<IReceipt, ReceiptModel>()
                 .ForMember(rm => rm.ReceiptDetailsIds, r => r.MapFrom(x => x.ReceiptDetails.Select(rd => rd.Id)))
                 .ReverseMap();
 
-            this.CreateMap<Product, ProductModel>()
-                .ForMember(cm => cm.Id, c => c.MapFrom(x => x.Id))
+            this.CreateMap<IProduct, ProductModel>()
                 .ForMember(pm => pm.ProductCategoryId, p => p.MapFrom(x => x.ProductCategoryId))
                 .ForMember(pm => pm.CategoryName, p => p.MapFrom(x => x.Category.CategoryName))
                 .ForMember(pm => pm.ReceiptDetailIds, p => p.MapFrom(x => x.ReceiptDetails.Select(rd => rd.Id)))
                 .ReverseMap();
 
-            this.CreateMap<ReceiptDetail, ReceiptDetailModel>()
-                .ForMember(cm => cm.Id, c => c.MapFrom(x => x.Id))
+            this.CreateMap<IReceiptDetail, ReceiptDetailModel>()
                 .ReverseMap();
 
-            this.CreateMap<Customer, CustomerModel>()
-                .ForMember(cm => cm.Id, c => c.MapFrom(x => x.Id))
+            this.CreateMap<ICustomer, CustomerModel>()
                 .ForMember(cm => cm.ReceiptsIds, c => c.MapFrom(x => x.Receipts.Select(r => r.Id)))
                 .ForMember(cm => cm.Name, c => c.MapFrom(x => x.Person.Name))
                 .ForMember(cm => cm.Surname, c => c.MapFrom(x => x.Person.Surname))
                 .ForMember(cm => cm.BirthDate, c => c.MapFrom(x => x.Person.BirthDate))
                 .ReverseMap();
 
-            this.CreateMap<ProductCategory, ProductCategoryModel>()
-                .ForMember(cm => cm.Id, c => c.MapFrom(x => x.Id))
+            this.CreateMap<IProductCategory, ProductCategoryModel>()
                 .ForMember(pcm => pcm.ProductIds, pc => pc.MapFrom(x => x.Products.Select(p => p.Id)))
                 .ReverseMap();
         }
