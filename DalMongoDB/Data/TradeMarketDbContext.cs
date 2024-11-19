5bb5ec38e128b69b626a3eb9d24c2fe1774eb4c9
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-11-19 15:55:44 +0200

    Implement UnitOfWork and dbContext for MongoDB

---
+++ DalMongoDB/Data/TradeMarketDbContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using DalMongoDB.Entities;
+using MongoDB.Driver;
+using Microsoft.Extensions.Configuration;
+
+namespace DalMongoDB.Data
+{
+    public class TradeMarketDbContext
+    {
+        private readonly IMongoDatabase database;
+
+        public TradeMarketDbContext()
+        {
+        }
+
+        public TradeMarketDbContext(IMongoClient mongoClient, string databaseName)
+        {
+            this.database = mongoClient.GetDatabase(databaseName);
+            //InitializeCollections();
+        }
+
+        public IMongoDatabase Database => this.database;
+
+        public IMongoCollection<Customer> Customers => database.GetCollection<Customer>("Customers");
+        public IMongoCollection<Person> Persons => database.GetCollection<Person>("Persons");
+        public IMongoCollection<Product> Products => database.GetCollection<Product>("Products");
+        public IMongoCollection<ProductCategory> ProductCategories => database.GetCollection<ProductCategory>("ProductCategories");
+        public IMongoCollection<Receipt> Receipts => database.GetCollection<Receipt>("Receipts");
+        public IMongoCollection<ReceiptDetail> ReceiptsDetails => database.GetCollection<ReceiptDetail>("ReceiptDetails");
+
+        //private void InitializeCollections()
+        //{
+        //    CreateCollectionIfNotExists<Customer>("Customers");
+        //    CreateCollectionIfNotExists<Person>("Persons");
+        //    CreateCollectionIfNotExists<Product>("Products");
+        //    CreateCollectionIfNotExists<ProductCategory>("ProductCategories");
+        //    CreateCollectionIfNotExists<Receipt>("Receipts");
+        //    CreateCollectionIfNotExists<ReceiptDetail>("ReceiptDetails");
+        //}
+
+        //private void CreateCollectionIfNotExists<T>(string collectionName)
+        //{
+        //    var collections = database.ListCollectionNames().ToList();
+
+        //    if (!collections.Contains(collectionName))
+        //    {
+        //        database.CreateCollection(collectionName);
+        //        Console.WriteLine($"Collection {collectionName} is created.");
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine($"Collection {collectionName} is already існує.");
+        //    }
+        //}
+    }
+}
---
+++ DalMongoDB/Data/UnitOfWork.cs
@@ -0,0 +1,41 @@
+﻿using System.Threading.Tasks;
+using DalMongoDB.Repositories;
+using Abstraction.IRepositories;
+using MongoDB.Driver;
+
+namespace DalMongoDB.Data
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IMongoDatabase database;
+
+        public UnitOfWork(IMongoDatabase database)
+        {
+            this.database = database;
+
+            this.CustomerRepository = new CustomerRepository(database);
+            this.PersonRepository = new PersonRepository(database);
+            this.ProductRepository = new ProductRepository(database);
+            this.ProductCategoryRepository = new ProductCategoryRepository(database);
+            this.ReceiptRepository = new ReceiptRepository(database);
+            this.ReceiptDetailRepository = new ReceiptDetailRepository(database);
+        }
+
+        public ICustomerRepository CustomerRepository { get; }
+
+        public IPersonRepository PersonRepository { get; }
+
+        public IProductRepository ProductRepository { get; }
+
+        public IProductCategoryRepository ProductCategoryRepository { get; }
+
+        public IReceiptRepository ReceiptRepository { get; }
+
+        public IReceiptDetailRepository ReceiptDetailRepository { get; }
+
+        public Task SaveAsync()
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
