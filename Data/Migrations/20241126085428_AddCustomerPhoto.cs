c7dfe3f9f726b05f85ddd815c67235acbd023ae4
Viktor <Viktor.Oleshek.PZ.2022@lpnu.ua>
2024-12-03 01:13:39 +0200

    Update DB migrations

---
+++ Data/Migrations/20241126085428_AddCustomerPhoto.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "Photo",
+                table: "Customer",
+                type: "varbinary(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Photo",
+                table: "Customer");
+        }
+    }
+}
